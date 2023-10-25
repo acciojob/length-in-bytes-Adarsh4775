7f3865c179c1a44a5f7cf8bcf838ffa8f93f9921
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-25 15:41:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	const encoder = new TextEncoder();
+	const encodedString = encoder.encode(str);
+	return encodedString.length;
 };
-
+byteSize();
 // Do not change the code below
 const str = prompt("Enter some string.");
-alert(byteSize(str));
+alert(byteSize(str));  
+
